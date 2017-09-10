4e3c5f5d72b1669435930add1863f3ae04d6680a
efrj <efrj@hotmail.com>
2017-09-09 21:28:37 -0300

    Correção da tabulação da rota index.

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import random
 
 @route('/')
 def index():
-    return template ('views/index.tpl', colors=['#cb3d00', '#336699', '#521515', '#478244', '#8f2525', '#4e5b6e', '#ff5a36', '#004b26', '#295398', '#6f0000'])
+	colors=['#cb3d00','#336699','#521515','#478244','#8f2525','#4e5b6e','#ff5a36','#004b26','#295398','#6f0000']
+	return template('views/index.tpl',colors=colors)
 
 run(host='localhost', port=8000)
