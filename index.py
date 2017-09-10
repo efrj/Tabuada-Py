86c21b8acfcabfcf272917a1cea23a065520371e
efrj <efrj@hotmail.com>
2017-09-10 11:27:32 -0300

    Configuração dos assets css.

--- index.py
+++ index.py
@@ -1,9 +1,16 @@
-from bottle import route, run, template
+from bottle import run, route, template, static_file
 import random
 
 @route('/')
 def index():
 	colors=['#cb3d00','#336699','#521515','#478244','#8f2525','#4e5b6e','#ff5a36','#004b26','#295398','#6f0000']
 	return template('views/index.tpl',colors=colors)
+
+@route('/assets/css/<filename>')
+def server_static(filename):
+	return static_file(filename, root='./assets/css')
+
+if __name__ == '__main__':
+	run(reloader=True, debug=True)
 
 run(host='localhost', port=8000)
