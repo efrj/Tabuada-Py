b40e6187c0268281203b091dab8b648342cf24c1
Edivaldo Reis <efrj@hotmail.com>
2017-09-17 20:12:35 -0300

    Adição da rota para gerar o arquivo da tabuada em PDF com a biblioteca pdfkit.

--- index.py
+++ index.py
@@ -1,10 +1,15 @@
 from bottle import run, route, template, static_file
 import random
+import pdfkit
 
 @route('/')
 def index():
 	colors=['#cb3d00','#336699','#521515','#478244','#8f2525','#4e5b6e','#ff5a36','#004b26','#295398','#6f0000']
 	return template('views/index.tpl',colors=colors)
+
+@route('/pdf')
+def pdf():
+	pdfkit.from_url('http://localhost:8080', 'tabuada.pdf')
 
 @route('/assets/css/<filename>')
 def server_static(filename):
